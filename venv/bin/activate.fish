32b87eee5adcb39b7d6452fe3e311e0d3df111e7
edwinfr <edwinfrcperez@gmail.com>
2026-07-31 11:23:09 -0600

    guardado con flask

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/edwin/Escritorio/pyth/venv
+set -gx VIRTUAL_ENV /home/edwin/Descargas/pyth/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
